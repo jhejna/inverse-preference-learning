be9b70fffa01d5269f4352c7b1349babf113e82b
Joey Hejna <joey.hejna@gmail.com>
2022-02-23 18:48:16 -0800

    fix ac policy refactor

--- research/networks/base.py
+++ research/networks/base.py
@@ -14,9 +14,10 @@ class ActorCriticPolicy(nn.Module):
         encoder_kwargs.update(kwargs)
         actor_kwargs.update(kwargs)
         critic_kwargs.update(kwargs)
+
+        self.encoder_class, self.encoder_kwargs = encoder_class, encoder_kwargs
         self.actor_class, self.actor_kwargs = actor_class, actor_kwargs
-        self.critic_class, critic_kwargs = critic_class, critic_kwargs
-        self.encoder_class, encoder_kwargs = encoder_class, encoder_kwargs
+        self.critic_class, self.critic_kwargs = critic_class, critic_kwargs
 
         self.reset_encoder()
         self.reset_actor()
@@ -26,19 +27,18 @@ class ActorCriticPolicy(nn.Module):
         encoder_class = vars(research.networks)[self.encoder_class] if isinstance(self.encoder_class, str) else self.encoder_class
         if encoder_class is not None:
             self._encoder = encoder_class(self.observation_space, self.action_space, **self.encoder_kwargs)
-            # Modify the observation space
-            if hasattr(self._encoder, "output_space"):
-                observation_space = self._encoder.output_space
         else:
             self._encoder = nn.Identity()
 
     def reset_actor(self):
+        observation_space = self.encoder.output_space if hasattr(self.encoder, "output_space") else self.observation_space
         actor_class = vars(research.networks)[self.actor_class] if isinstance(self.actor_class, str) else self.actor_class
-        self._actor = actor_class(self.observation_space, self.action_space, **self.actor_kwargs)
+        self._actor = actor_class(observation_space, self.action_space, **self.actor_kwargs)
 
     def reset_critic(self):
+        observation_space = self.encoder.output_space if hasattr(self.encoder, "output_space") else self.observation_space
         critic_class = vars(research.networks)[self.critic_class] if isinstance(self.critic_class, str) else self.critic_class
-        self._critic = critic_class(self.observation_space, self.action_space, **self.critic_kwargs)
+        self._critic = critic_class(observation_space, self.action_space, **self.critic_kwargs)
 
     @property
     def actor(self):
