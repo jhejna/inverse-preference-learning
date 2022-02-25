1f9ee25f05e964116f8895008d6b016a2c05bb70
Joey Hejna <joey.hejna@gmail.com>
2022-02-24 16:29:34 -0800

    add cleanup tool for tmp

---
+++ tools/cleanup.py
@@ -0,0 +1,25 @@
+# This script cleans up all the temporary files used by the research codebase.
+
+import os
+import shutil
+
+base_path = "/tmp/"
+
+job_scripts_removed = 0
+replay_buffers_removed = 0
+
+for name in os.listdir(base_path):
+    path = os.path.join(base_path, name)
+    try:
+        if name.startswith("job_"):
+            os.remove(path)
+            job_scripts_removed += 1
+        elif name.startswith("replay_buffer_"):
+            shutil.rmtree(path)
+            replay_buffers_removed += 1
+    except:
+        continue
+
+print("Finished Cleanup.")
+print("Removed", job_scripts_removed, "job scripts.")
+print("Removed", replay_buffers_removed, "replay buffers.")
