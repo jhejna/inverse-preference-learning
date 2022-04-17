6c37841345083aaddc5c001dd39917df93a6c075
Joey Hejna <joey.hejna@gmail.com>
2022-04-16 18:26:16 -0700

    add normalization to PPO implementation

--- research/algs/ppo.py
+++ research/algs/ppo.py
@@ -1,3 +1,4 @@
+from re import L
 import torch
 import numpy as np
 import itertools
@@ -6,7 +7,9 @@ import collections
 
 from .base import Algorithm
 from research.utils import utils
+from research.processors.normalization import RunningMeanStd, RunningObservationNormalizer
 from research.networks.base import ActorCriticPolicy
+from research.datasets import RolloutBuffer
 
 class PPO(Algorithm):
     
@@ -18,9 +21,15 @@ class PPO(Algorithm):
             vf_coeff=0.5,
             max_grad_norm=0.5,
             normalize_advantage=True,
+            normalize_returns=False,
+            reward_clip=None,            
             **kwargs
             ):
         super().__init__(*args, **kwargs)
+        # Perform initial type checks
+        assert isinstance(self.network, ActorCriticPolicy)
+
+        # Store algorithm values
         self.num_epochs = num_epochs
         self.clip_range = clip_range
         self.clip_range_vf = clip_range_vf
@@ -28,6 +37,12 @@ class PPO(Algorithm):
         self.vf_coeff = vf_coeff
         self.max_grad_norm = max_grad_norm
         self.normalize_advantage = normalize_advantage
+
+        # Return normalization
+        self.normalize_returns = normalize_returns
+        self.reward_clip = reward_clip
+        if self.normalize_returns:
+            self.return_rms = RunningMeanStd(shape=(), dtype=np.float64)
 
         # Losses
         self.value_criterion = torch.nn.MSELoss()
@@ -39,9 +54,11 @@ class PPO(Algorithm):
 
         # Setup metrics
         metrics = dict(reward=[], length=[], success=[])
-        ep_reward, ep_length, ep_success = 0, 0, False
+        ep_reward, ep_length, ep_return, ep_success = 0, 0, 0, False
 
         obs = self.env.reset()
+        if isinstance(self.processor, RunningObservationNormalizer):
+            self.processor.update(obs)
         self.dataset.add(obs=obs) # Add the first observation
         while not self.dataset.is_full:
             
@@ -57,13 +74,24 @@ class PPO(Algorithm):
                 action = utils.to_np(utils.get_from_batch(action, 0))
                 log_prob = utils.to_np(utils.get_from_batch(log_prob, 0))
                 value = utils.to_np(utils.get_from_batch(value, 0))
+                extras = self._compute_extras(dist)
             
             if isinstance(self.env.action_space, gym.spaces.Box): # Clip the actions
                 clipped_action = np.clip(action, self.env.action_space.low, self.env.action_space.high)
+            
             obs, reward, done, info = self.env.step(clipped_action)
+            if isinstance(self.processor, RunningObservationNormalizer):
+                self.processor.update(obs)
             
             ep_reward += reward
             ep_length += 1
+            ep_return = self.dataset.discount*ep_return + reward
+            if self.normalize_returns:
+                self.return_rms.update(ep_return)
+                reward = reward / self.return_rms.std
+                if self.reward_clip is not None:
+                    reward = np.clip(reward, -self.reward_clip, self.reward_clip)
+
             if ('success' in info and info['success']) or ('is_success' in info and info['is_success']):
                 ep_success = True
             
@@ -72,7 +100,7 @@ class PPO(Algorithm):
                 metrics['reward'].append(ep_reward)
                 metrics['length'].append(ep_length)
                 metrics['success'].append(ep_success)
-                ep_reward, ep_length, ep_success = 0, 0, False
+                ep_reward, ep_length, ep_return, ep_success = 0, 0, 0, False
                 # If its done, we need to update the observation as well as the terminal reward
                 with torch.no_grad():
                     batch = self._format_batch(utils.unsqueeze(obs, 0)) # Preprocess obs
@@ -82,7 +110,7 @@ class PPO(Algorithm):
                 obs = self.env.reset()
 
             # Note: Everything is from the last observation except for the observation, which is really next_obs
-            self.dataset.add(obs=obs, action=action, reward=reward, done=done, value=value, log_prob=log_prob)
+            self.dataset.add(obs=obs, action=action, reward=reward, done=done, value=value, log_prob=log_prob, **extras)
             
             self._env_steps += 1
 
@@ -91,7 +119,13 @@ class PPO(Algorithm):
         metrics['reward_std'] = np.std(metrics['reward'])
         return metrics
 
+    def _compute_extras(self, dist):
+        # Used for computing extras values for different versions of PPO
+        return {}
+
     def _setup_train(self):
+        # Checks
+        assert isinstance(self.dataset, RolloutBuffer)
         # Logging metrics
         self._env_steps = 0
         self._collect_rollouts()
@@ -143,91 +177,20 @@ class PPO(Algorithm):
 class AdaptiveKLPPO(Algorithm):
     
     def __init__(self, *args, 
-            num_epochs=10,
-            clip_range_vf=None,
-            ent_coeff=0.0,
-            vf_coeff=0.5,
-            max_grad_norm=0.5,
-            normalize_advantage=True,
             target_kl=0.025,
             kl_window=None,
             **kwargs
             ):
         super().__init__(*args, **kwargs)
-        self.num_epochs = num_epochs
-        self.clip_range_vf = clip_range_vf
-        self.ent_coeff = ent_coeff
-        self.vf_coeff = vf_coeff
-        self.max_grad_norm = max_grad_norm
-        self.normalize_advantage = normalize_advantage
+        assert self.clip_range is None, "Clip range is not used in Adaptive KL based PPO"
         self.target_kl = target_kl
         self.kl_window = kl_window
         self.beta = 1
 
-        # Losses
-        self.value_criterion = torch.nn.MSELoss()
-
-    def _collect_rollouts(self):
-        # Setup the dataset and network
-        self.dataset.setup()
-        self.eval_mode()
-
-        # Setup metrics
-        metrics = dict(reward=[], length=[], success=[])
-        ep_reward, ep_length, ep_success = 0, 0, False
-
-        obs = self.env.reset()
-        self.dataset.add(obs=obs) # Add the first observation
-        while not self.dataset.is_full:
-            
-            with torch.no_grad():
-                batch = self._format_batch(utils.unsqueeze(obs, 0)) # Preprocess obs
-                latent = self.network.encoder(batch)
-                dist = self.network.actor(latent)
-                # Collect relevant information
-                action = dist.sample()
-                log_prob = dist.log_prob(action).sum(dim=-1)
-                value = self.network.critic(latent)                
-                # Unprocess back to numpy
-                action = utils.to_np(utils.get_from_batch(action, 0))
-                log_prob = utils.to_np(utils.get_from_batch(log_prob, 0))
-                value = utils.to_np(utils.get_from_batch(value, 0))
-                # Additionally collect the mean and std for our actions
-                mu = utils.to_np(utils.get_from_batch(dist.loc, 0))
-                sigma = utils.to_np(utils.get_from_batch(dist.scale, 0))
-            
-            if isinstance(self.env.action_space, gym.spaces.Box): # Clip the actions
-                clipped_action = np.clip(action, self.env.action_space.low, self.env.action_space.high)
-            obs, reward, done, info = self.env.step(clipped_action)
-            
-            ep_reward += reward
-            ep_length += 1
-            if ('success' in info and info['success']) or ('is_success' in info and info['is_success']):
-                ep_success = True
-            
-            if done:
-                # Update metrics
-                metrics['reward'].append(ep_reward)
-                metrics['length'].append(ep_length)
-                metrics['success'].append(ep_success)
-                ep_reward, ep_length, ep_success = 0, 0, False
-                # If its done, we need to update the observation as well as the terminal reward
-                with torch.no_grad():
-                    batch = self._format_batch(utils.unsqueeze(obs, 0)) # Preprocess obs
-                    terminal_value = self.network.critic(self.network.encoder(batch))
-                    terminal_value = utils.to_np(utils.get_from_batch(terminal_value, 0))
-                reward += self.dataset.discount * terminal_value
-                obs = self.env.reset()
-
-            # Note: Everything is from the last observation except for the observation, which is really next_obs
-            self.dataset.add(obs=obs, action=action, reward=reward, done=done, value=value, log_prob=log_prob, mu=mu, sigma=sigma)
-            
-            self._env_steps += 1
-
-        self.train_mode()
-        metrics['env_steps'] = self._env_steps # Log environment steps because it isn't proportional to the number of batches.
-        metrics['reward_std'] = np.std(metrics['reward'])
-        return metrics
+    def _compute_extras(self, dist):
+        mu = utils.to_np(utils.get_from_batch(dist.loc, 0))
+        sigma = utils.to_np(utils.get_from_batch(dist.scale, 0))
+        return dict(mu=mu, sigma=sigma)
 
     def _setup_train(self):
         # Logging metrics
